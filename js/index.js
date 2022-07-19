9fb3dceef3f06b169b261bb591ced6d78aa7cea1
Julio Garrido Vico <juliogarridovico@gmail.com>
2022-07-19 20:16:27 +0200

    itration 1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 console.log("I'm ready!");
 // Iteration 1: Names and Input
 //
+let hacker1 = "Julio"
+let hacker2 = "test"
+console.log("The driver's name is "+hacker1)
+console.log("The navigator's name is  "+hacker2)
 // Iteration 2: Conditionals
 
 
